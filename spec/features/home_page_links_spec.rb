012147f06d74bbe041702737ff094287277ec2d3
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-12 00:03:54 +0530

    adds links to navbar

--- spec/features/home_page_links_spec.rb
+++ spec/features/home_page_links_spec.rb
@@ -5,5 +5,10 @@ feature "Visit Home Page" do
         visit root_path
 
         expect(page).to have_content("Fleet Management")
+        expect(page).to have_link("Upload")
+        expect(page).to have_link("Vehicles")
+        expect(page).to have_link("Search")
+        expect(page).to have_link("CustomerReport")
+        expect(page).to have_link("OdometerReport")
     end
 end
