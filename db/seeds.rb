f91ac77336877a65f3024d6b651892f2a9c41aad
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-29 23:41:25 +0530

    feat: updated seeds.rb file with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+9.times do |number|
+  Report.create(
+    name: "Report #{number}",
+    year: 2010 + number,
+    position: number,
+    image: "https://place-hold.it/300x300",
+  )
+end
